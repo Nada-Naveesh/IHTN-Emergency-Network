aa80d0c4727b59b003fbeff535fddd301982d892
NADA NAVEESH <126503897+Nada-Naveesh@users.noreply.github.com>
2026-05-23 17:23:26 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+from routes.emergency import emergency_bp
 
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(emergency_bp)
+
+@app.route("/")
+def home():
+    return {"message": "IHTN Backend Running on Render"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
