3a51e35b5b8c71aba9d34827decd359fb1157ecb
NADA NAVEESH <126503897+Nada-Naveesh@users.noreply.github.com>
2026-05-23 17:26:18 +0530

    Update triage.py

--- backend/service/triage.py
+++ backend/service/triage.py
@@ -1,1 +1,11 @@
+def predict_severity(symptoms: str):
+    s = symptoms.lower()
 
+    if "chest pain" in s or "breathing" in s or "unconscious" in s:
+        return "CRITICAL"
+    elif "accident" in s or "bleeding" in s:
+        return "HIGH"
+    elif "fever" in s:
+        return "MODERATE"
+    else:
+        return "LOW"
