381910039cd0e7519184f3f4e5d09d351ee60345
NADA NAVEESH <126503897+Nada-Naveesh@users.noreply.github.com>
2026-05-23 17:27:06 +0530

    Update hospital.py

--- backend/service/hospital.py
+++ backend/service/hospital.py
@@ -1,1 +1,9 @@
-
+def get_hospital(severity: str):
+    if severity == "CRITICAL":
+        return "City Trauma Hospital (ICU Ready)"
+    elif severity == "HIGH":
+        return "Emergency Care Hospital"
+    elif severity == "MODERATE":
+        return "General Hospital"
+    else:
+        return "Primary Health Center"
